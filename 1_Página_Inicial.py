26b219b0c02c9b2ed96db98b66a973e11d57589d
M-Ortiz <69127852+MateusOF5512@users.noreply.github.com>
2022-11-08 18:09:35 -0300

    Update 1_Página_Inicial.py

--- 1_Página_Inicial.py
+++ 1_Página_Inicial.py
@@ -7,14 +7,8 @@ im = Image.open("instagram.png")
 st.set_page_config(page_title="Instagram Monitor", page_icon=im, layout="wide")
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 
-from layout.pages_brasil2 import *
-from layout.teste_pages3 import *
 
 
-# APLICAÇÃO
-topo()
-
-st.markdown("# Insta Monitor User 1")
-st.sidebar.markdown("# Insta Monitor:")
+st.markdown("# Bem vindo ao Insta Monitor Business")
 
 
